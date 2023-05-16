820458333e1f0fbf2289b897f793eb1f0e8fa047
faxcon08 <faxcon-08@mail.ru>
2023-05-16 21:00:38 +0300

    add new branch

---
+++ src/main/java/ru/skypro/mvc/homework31_2/repository/EmployeeRepositoryImpl.java
@@ -0,0 +1,44 @@
+package ru.skypro.mvc.homework31_2.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.skypro.mvc.homework31_2.pojo.Employee;
+
+import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
+
+@Repository
+public class EmployeeRepositoryImpl implements EmployeeRepository{
+    private final List<Employee> employeesList = List.of(
+            new Employee("Катя",90_000),
+            new Employee("Дима",102_000),
+            new Employee("Олег", 80_000),
+            new Employee("Сергей", 50_000),
+            new Employee("Владимир", 85_000),
+            new Employee("Вика", 165_000));
+@Override
+    public List<Employee> getAllEmployee() {
+        return employeesList;
+    }
+
+    @Override
+    public String getSalarySum() {
+        return "Sum of Salary = "+employeesList.stream().map(x->x.getSalary()).mapToInt(y->y.intValue()).sum();
+    }
+
+    @Override
+    public String getMinSalary() {
+        return "Min salary = "+employeesList.stream().map(x->x.getSalary()).mapToInt(y->y.intValue()).min().orElse(0);
+    }
+
+    @Override
+    public String getMaxSalary() {
+        return "MAX salary = "+employeesList.stream().map(x->x.getSalary()).mapToInt(y->y.intValue()).max().orElse(0);
+    }
+
+    @Override
+    public List<Employee> getEmployeeWithHighSalary() {
+        OptionalDouble averageSalary = employeesList.stream().map((x)->x.getSalary()).mapToInt(y->y.intValue()).average();
+        return employeesList.stream().filter(x->x.getSalary()>=averageSalary.getAsDouble()).collect(Collectors.toList());
+    }
+}
