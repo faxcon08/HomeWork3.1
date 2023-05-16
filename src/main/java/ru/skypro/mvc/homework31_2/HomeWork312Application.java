3c958d0d953dcd6af6d66115ed5ddc5645886a82
faxcon08 <faxcon-08@mail.ru>
2023-05-16 20:53:20 +0300

    initial commit

---
+++ src/main/java/ru/skypro/mvc/homework31_2/HomeWork312Application.java
@@ -0,0 +1,13 @@
+package ru.skypro.mvc.homework31_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork312Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork312Application.class, args);
+    }
+
+}
